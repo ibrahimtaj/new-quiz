c7b9c86543446d01a4963f33925f33d030826e7e
ibrahimtaj <taj_it@yahoo.com>
2020-06-17 16:53:14 +0530

    Updates

---
+++ precache-manifest.253a68e655c38fd5bcda4956456668af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0994045d8ba848890b67aa8351fc00",
+    "url": "/new-quiz/index.html"
+  },
+  {
+    "revision": "27e01baeb6508c9bd887",
+    "url": "/new-quiz/static/css/main.b830b46c.chunk.css"
+  },
+  {
+    "revision": "17e2a7c8543faa9f36b3",
+    "url": "/new-quiz/static/js/2.1223bcd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-quiz/static/js/2.1223bcd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e01baeb6508c9bd887",
+    "url": "/new-quiz/static/js/main.c37ab7b2.chunk.js"
+  },
+  {
+    "revision": "87b074cfc6eae0926a0f",
+    "url": "/new-quiz/static/js/runtime-main.9ae38e55.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-quiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
